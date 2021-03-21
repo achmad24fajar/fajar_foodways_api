89f48abe58d92333c99693c30a7affa7c5cd2ef3
achmadfajar-24 <achmad24fajar@gmail.com>
2021-03-21 12:00:17 +0700

    add product again

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const {getUsers, deleteUser} = require('../controller/UserController');
 const {getProducts, getProductsByUser, getDetailProduct} = require('../controller/ProductController');
 const {registerUser} = require('../controller/AuthController');
+const { uploadFile } = require("../middlewares/upload");
 
 router.get('/users', getUsers);
 router.delete('/users/:id', deleteUser);
@@ -13,6 +14,6 @@ router.get('/products', getProducts);
 router.get('/products/:id', getProductsByUser);
 router.get('/product/:id', getDetailProduct);
 
-router.post('/register', registerUser);
+router.post('/register', uploadFile("imageFile", "videoFile"), registerUser);
 
 module.exports = router
